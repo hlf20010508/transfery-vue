aa436d045793a09d734c8354f223d85bebcaab23
L-ING <hlf01@icloud.com>
2024-02-08 19:39:18 +0800

    fix: setTimeout误写成了setInterval

--- src/hooks/refresh.js
+++ src/hooks/refresh.js
@@ -9,6 +9,6 @@ import { showRefreshSpin } from "@/stores/refresh.js";
 
 export function refreshPage() {
     showRefreshSpin.value = true;
-    //延迟0.5秒刷新，让loading图标能够显示全，不然只有一个点在转
-    setInterval(() => location.reload(), 500);
+    //延迟0.5秒刷新，让loading图标能够显示全
+    setTimeout(() => location.reload(), 500);
 }
