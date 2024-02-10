cd554566afd01b737e4cc2d90ac5c60080338390
L-ING <hlf01@icloud.com>
2024-02-10 15:16:57 +0800

    chore: 使用相对路径访问静态资源

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
+    base: './',
     plugins: [
       vue(),
     ],
