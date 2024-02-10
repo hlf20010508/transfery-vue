9b6bbb84c7d8f5f0cf5e1281649722cee4e3065e
L-ING <hlf01@icloud.com>
2024-02-10 15:33:13 +0800

    perf: 删除updateMessageAreaHeight的nextTick

--- src/hooks/message.js
+++ src/hooks/message.js
@@ -25,25 +25,23 @@ export function messageAreaScrollToBottom() {
 }
 
 export function updateMessageAreaHeight() {
-    nextTick(() => {
-        // 更新message-area高度
-        // 获取可视区域高度
-        let visualHeight = window.visualViewport.height;
-        let titleBarHeight = jquery("#title-bar").outerHeight(true);
-        let controlBarHeight = jquery("#control-bar").outerHeight(true);
-        let inputAreaHeight = jquery("#input-area").outerHeight(true);
-        let messageAreaHeight = visualHeight - titleBarHeight - controlBarHeight - inputAreaHeight;
-        jquery("#message-area").outerHeight(messageAreaHeight);
+    // 更新message-area高度
+    // 获取可视区域高度
+    let visualHeight = window.visualViewport.height;
+    let titleBarHeight = jquery("#title-bar").outerHeight(true);
+    let controlBarHeight = jquery("#control-bar").outerHeight(true);
+    let inputAreaHeight = jquery("#input-area").outerHeight(true);
+    let messageAreaHeight = visualHeight - titleBarHeight - controlBarHeight - inputAreaHeight;
+    jquery("#message-area").outerHeight(messageAreaHeight);
 
-        // 更新empty图标大小
-        let appWidth = jquery("#app").width();
-        // 使用较小长度的一半
-        let emptyIconSize = appWidth < messageAreaHeight ? parseInt(appWidth / 2) : parseInt(messageAreaHeight / 2);
-        jquery(".n-empty__icon").css("--n-icon-size", emptyIconSize + "px");
+    // 更新empty图标大小
+    let appWidth = jquery("#app").width();
+    // 使用较小长度的一半
+    let emptyIconSize = appWidth < messageAreaHeight ? parseInt(appWidth / 2) : parseInt(messageAreaHeight / 2);
+    jquery(".n-empty__icon").css("--n-icon-size", emptyIconSize + "px");
 
-        // 苹果：输入法获取焦点时window高度不变，整体向上移动
-        // 安卓：呼出键盘时改变window高度
-        if (device.ios()) window.scrollTo(0, 0); // 滚动到页面最上方
-        if (device.mobile() || device.tablet()) messageAreaScrollToBottom();
-    })
+    // 苹果：输入法获取焦点时window高度不变，整体向上移动
+    // 安卓：呼出键盘时改变window高度
+    if (device.ios()) window.scrollTo(0, 0); // 滚动到页面最上方
+    if (device.mobile() || device.tablet()) messageAreaScrollToBottom();
 }
